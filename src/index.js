2601b1fc7a6aa31ca61dbc430c081a49fc89aa8e
Jatin-Barman <barmanjatinswain@gmail.com>
2024-01-10 00:48:17 +0530

    Optimizing the API  error and response in utils

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 // require("dotenv").config({ path: "./env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(app.listen(process.env.PORT) || 8000, () => {
+    console.log(`App is running at port : ${process.env.PORT || 8000}`);
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed !!!", err);
+  });
+
+///Assignment Done///
+app.on("error", (error) => {
+  console.log("Error:", error);
+  throw error;
+});
 
 /*
 // A way to connect DB in this file itself
