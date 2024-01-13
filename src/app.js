ba67e01a2d32bf87a56a65c52bdadb25761278f9
Jatin-Barman <barmanjatinswain@gmail.com>
2024-01-13 23:39:22 +0530

    changes added

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //routes import
-import userRouter from "./routes/user.routes.j";
+import userRouter from "./routes/user.routes.js";
 
 //route declaration
 app.use("/api/v1/users", userRouter);
